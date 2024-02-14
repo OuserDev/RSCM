ced36c7f113f71add827a55b81988ba28e64ffa0
Miku-zzAng <hako5460@naver.com>
2024-02-14 16:51:38 +0900

    폴리곤 1차 구현 02

--- src/components/rscmMap.vue
+++ src/components/rscmMap.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <h1>카카오맵 보기</h1>
+    <h1>RSCM (RealTime-SNS-Crime-Map)</h1>
     <div id="map"></div>
   </div>
 </template>
 
 <style scope>
 #map {
-  width: 100%;
+  width: 50%;
   height: 800px;
 }
 body {
@@ -20,6 +20,14 @@ body {
   padding: 10px;
   border-radius: 10px;
   color: #fff;
+}
+.label {
+  /* Label 스타일 정의 */
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 4px;
+  padding: 4px;
+  font-size: 14px;
+  /* 추가 스타일 */
 }
 </style>
 
@@ -92,7 +100,6 @@ export default {
           paths.push(path); // paths 배열에 현재 polygon의 path를 추가
         });
       }
-
       // 모든 path에 대해 폴리곤 설정
       paths.forEach((path) => {
         this.setPolygon(path, feature.properties);
@@ -102,17 +109,39 @@ export default {
       const polygon = new kakao.maps.Polygon({
         path: path,
         strokeWeight: 2,
-        strokeColor: "#004c80",
+        strokeColor: "#003478",
         strokeOpacity: 0.8,
         fillColor: "#fff",
         fillOpacity: 0.7,
       });
+      // 폴리곤의 중심점을 계산
+      const centroidPoint = this.centroid(path);
+      //   this.createLabel(centroidPoint, properties.CTP_KOR_NM);
+      //   // 폴리곤 중심에 CustomOverlay를 생성하여 지역 이름 표시
+      //   const content = document.createElement("p");
+      //   content.className = "overlaybox";
+      //   content.innerText = properties.CTP_KOR_NM;
+
+      //   const overlay = new kakao.maps.CustomOverlay({
+      //     map: this.map,
+      //     position: centroidPoint,
+      //     content: content,
+      //     yAnchor: 1,
+      //   });
+
+      //   // 중심점과 지역 이름을 콘솔에 로깅
+      //   console.log(
+      //     `지역: ${
+      //       properties.CTP_KOR_NM
+      //     }, 중심점: ${centroidPoint.getLat()}, ${centroidPoint.getLng()}`
+      //   );
 
       kakao.maps.event.addListener(polygon, "mouseover", () => {
         polygon.setOptions({ fillColor: "#09f" });
-        this.customOverlay.setContent(
-          `<div class='overlaybox'>${properties.CTP_KOR_NM}</div>`
-        );
+        this.customOverlay.setPosition(centroidPoint); // centroid 계산 로직에 path를 전달
+        // this.customOverlay.setContent(
+        //   `<div class='overlaybox'>${properties.CTP_KOR_NM}</div>`
+        // );
         this.customOverlay.setMap(this.map);
       });
 
@@ -123,6 +152,59 @@ export default {
 
       polygon.setMap(this.map);
     },
+    centroid(points) {
+      let x = 0,
+        y = 0,
+        area = 0,
+        factor = 0;
+
+      for (let i = 0, j = points.length - 1; i < points.length; j = i, i++) {
+        let xi = points[i].getLng(),
+          yi = points[i].getLat();
+        let xj = points[j].getLng(),
+          yj = points[j].getLat();
+        factor = xi * yj - xj * yi;
+        x += (xi + xj) * factor;
+        y += (yi + yj) * factor;
+        area += factor * 3;
+      }
+
+      // If area is 0, which shouldn't happen for polygons, return early to avoid division by zero
+      if (area === 0) {
+        return new kakao.maps.LatLng(0, 0); // Or some other default value
+      }
+
+      area = Math.abs(area);
+      x /= area;
+      y /= area;
+
+      // Adjust the signs of x and y as they should be negative if area is negative
+      if (area < 0) {
+        x = -x;
+        y = -y;
+      }
+
+      return new kakao.maps.LatLng(y, x);
+    },
+    // createLabel(centroid, text) {
+    //   var content = document.createElement("div");
+    //   content.className = "label"; // CSS 클래스 설정
+    //   content.innerHTML = text;
+    //   content.style.position = "relative";
+    //   content.style.left = "-50%";
+    //   content.style.top = "-50%";
+    //   content.style.whiteSpace = "nowrap";
+    //   content.style.textAlign = "center";
+
+    //   var customOverlay = new kakao.maps.CustomOverlay({
+    //     map: this.map,
+    //     position: centroid,
+    //     content: content,
+    //     yAnchor: 1,
+    //   });
+
+    //   return customOverlay;
+    // },
   },
 };
 </script>
