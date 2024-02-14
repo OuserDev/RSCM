db87427a4cf5a5f68160fa74a87f2812f3bfaa41
Miku-zzAng <hako5460@naver.com>
2024-02-14 15:10:35 +0900

    폴리곤 1차 구현

--- src/components/rscmMap.vue
+++ src/components/rscmMap.vue
@@ -25,20 +25,21 @@ body {
 
 <script>
 import axios from "axios";
-import sidoJson from "@/assets/sidoJson.json";
+import sidoJson from "@/assets/TL_SCCO_CTPRVN.json";
 
 export default {
   name: "rscmMap",
   data() {
     return {
       map: null,
+      customOverlay: null, // 초기화를 여기서 하지 않음
     };
   },
   setup() {},
   created() {},
   mounted() {
     if (window.kakao && window.kakao.maps) {
-      this.loadMap();
+      this.loadMap({ mapId: "map" });
     } else {
       this.loadScript();
     }
@@ -50,17 +51,26 @@ export default {
       const script = document.createElement("script");
       script.src =
         "https://dapi.kakao.com/v2/maps/sdk.js?appkey=c1479b041c5feb85f1cc85dc2ed3cf39&autoload=false";
-      script.onload = () => window.kakao.maps.load(this.loadMap);
+      // 화살표 함수를 사용하여 this.loadMap을 호출하고, 필요한 매개변수를 전달한다.
+      script.onload = () =>
+        window.kakao.maps.load(() => this.loadMap({ mapId: "map" }));
       document.head.appendChild(script);
     },
-    loadMap() {
-      // 맵 출력하기
-      const container = document.getElementById("map");
-      const options = {
-        center: new window.kakao.maps.LatLng(36.26682, 127.57865),
-        level: 12,
+    loadMap(options) {
+      // CustomOverlay 인스턴스 생성
+      this.customOverlay = new kakao.maps.CustomOverlay({});
+
+      // 지도를 표시할 컨테이너
+      const container = document.getElementById(options.mapId || "map"); // options.mapId를 사용하거나 기본값으로 "map" 사용
+      const mapOptions = {
+        center: new kakao.maps.LatLng(36.26682, 127.57865), // 지도의 중심좌표
+        level: 12, // 지도의 확대 레벨
       };
-      this.map = new window.kakao.maps.Map(container, options);
+
+      // 지도 생성
+      this.map = new kakao.maps.Map(container, mapOptions);
+
+      // 폴리곤 데이터 로드 및 지도에 표시
       this.loadPolygonData();
     },
     loadPolygonData() {
@@ -69,27 +79,49 @@ export default {
       });
     },
     getPolyCode(feature) {
-      const coordinates = feature.geometry.coordinates;
+      // coordinates 배열을 순회하여 모든 좌표를 처리
       const type = feature.geometry.type;
-      const properties = feature.properties;
-      let path = []; // kakao.maps.LatLng 객체의 배열
-      let points = []; // centroid 계산을 위한 x, y 좌표 객체의 배열
+      let paths = []; // 여러 path를 저장할 배열
 
       if (type === "Polygon") {
-        coordinates[0].forEach((coord) => {
-          path.push(new kakao.maps.LatLng(coord[1], coord[0]));
-          points.push({ x: coord[0], y: coord[1] });
-        });
-      } else if (type === "MultiPolygon") {
-        coordinates.forEach((polygon) => {
-          polygon[0].forEach((coord) => {
+        feature.geometry.coordinates.forEach((polygon) => {
+          let path = []; // 각 Polygon의 path
+          polygon.forEach((coord) => {
             path.push(new kakao.maps.LatLng(coord[1], coord[0]));
-            points.push({ x: coord[0], y: coord[1] });
           });
+          paths.push(path); // paths 배열에 현재 polygon의 path를 추가
         });
       }
-      // Set the polygon
-      this.setPolygon(path, properties);
+
+      // 모든 path에 대해 폴리곤 설정
+      paths.forEach((path) => {
+        this.setPolygon(path, feature.properties);
+      });
+    },
+    setPolygon(path, properties) {
+      const polygon = new kakao.maps.Polygon({
+        path: path,
+        strokeWeight: 2,
+        strokeColor: "#004c80",
+        strokeOpacity: 0.8,
+        fillColor: "#fff",
+        fillOpacity: 0.7,
+      });
+
+      kakao.maps.event.addListener(polygon, "mouseover", () => {
+        polygon.setOptions({ fillColor: "#09f" });
+        this.customOverlay.setContent(
+          `<div class='overlaybox'>${properties.CTP_KOR_NM}</div>`
+        );
+        this.customOverlay.setMap(this.map);
+      });
+
+      kakao.maps.event.addListener(polygon, "mouseout", () => {
+        polygon.setOptions({ fillColor: "#fff" });
+        this.customOverlay.setMap(null);
+      });
+
+      polygon.setMap(this.map);
     },
   },
 };
